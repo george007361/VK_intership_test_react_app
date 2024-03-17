95972acde91971fc4e625bf61297768fc34ec425
George007361 <gera.illarionov@mail.ru>
2024-03-17 19:47:28 +0300

    install deps and app icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { ConfigProvider, AdaptivityProvider, AppRoot } from '@vkontakte/vkui'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { App } from './App';
+import '@vkontakte/vkui/dist/vkui.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ConfigProvider appearance="light">
+      <AdaptivityProvider>
+        <AppRoot>
+          <App />
+        </AppRoot>
+      </AdaptivityProvider>
+    </ConfigProvider>
+  </QueryClientProvider>
+  // </React.StrictMode>,
 )
